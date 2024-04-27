f8198b48a56346d6ea44079ded482b2738d0b4d0
Sukhrob Mukhammadiev <suhrobmuhammadiyev@gmail.com>
2024-04-27 18:47:39 +0500

    add myFirstReact

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import ReactLogo from '../src/img/React-icon.svg.png'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world!</h1>
+      <img src= {ReactLogo}/>
+    </div>
+  );
+}
+
+export default App;
